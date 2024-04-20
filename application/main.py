b12c44bf84a19a2eab74c0586c3f82493e71a6f7
Demi-Play <chubenkodo@gmail.com>
2024-04-20 17:35:53 +0300

    SalesPoint and Product

--- application/main.py
+++ application/main.py
@@ -1,6 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
-from db import Product
+from db import Product, SalesPoint
 from flasgger import Swagger
 from flask_sqlalchemy  import SQLAlchemy
 
@@ -10,67 +10,97 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///soliuz.db'
 
 DB = SQLAlchemy(app)
 
-
 swagger = Swagger(app)
 
-class HelloWorld(Resource):
-    """
-    This is the description for HelloWorld resource
-    """
-
-    def get(self):
-        """
-        This is the description for GET method
-        It returns a simple Hello World message
-        ---
-        responses:
-          200:
-            description: A simple hello world message
-        """
+@app.route('/products', methods=['GET', 'POST'])
+def products():
+    # render all
+    if request == 'GET':
         products = DB.session.query(Product).all()
         product_list = []
         for product in products:
-            user_dict = {
+            product_dict = {
                 'id': product.id,
                 'name': product.name,
                 'expiry_date': product.expiry_date,
                 'volume': product.volume,
                 'weight': product.weight
             }
-            product_list.append(user_dict)
+            product_list.append(product_dict)
         return jsonify(product_list)
+    # create
+    if request == 'POST':
+        product = request.get_json()
+        new_product = Product(name=product['name'], 
+                expiry_date=product['expiry_date'], 
+                volume=['volume'], 
+                weight=product['weight'])
+        DB.session.add(new_product)
+        DB.session.commit()
+        return {'status': 200}
+
+@app.route('/product/edit/<int:id>', methods=['POST', 'DELETE'])
+def product(id):
+    # edit
+    if request == 'POST':
+        product = request.get_json()
+        edit = DB.session.query(Product).filter_by(id=id).first()
+        edit.name=product['name']
+        edit.expiry_date=product['expiry_date'] 
+        edit.volume=['volume']
+        edit.weight=product['weight']
+        DB.session.add(edit)
+        DB.session.commit()
+        return {'status': 200}
+    # delete
+    elif request == 'DELETE':
+        edit = DB.session.query(Product).filter_by(id=id).first()
+        DB.session.delete(edit)
+        DB.session.commit()
+        return {'status': 200}
 
 
-class Add(Resource):
-    """
-    This is the description for Add resource
-    """
-
-    def post(self, num1, num2):
-        """
-        This is the description for POST method
-        It adds two numbers and returns the result
-        ---
-        parameters:
-          - name: num1
-            in: path
-            type: int
-            required: true
-            description: The first number
-          - name: num2
-            in: path
-            type: int
-            required: true
-            description: The second number
-        responses:
-          200:
-            description: The sum of two numbers
-        """
-        return {"result": num1 + num2}
-
-
-api.add_resource(HelloWorld, "/")
-api.add_resource(Add, "/add/<int:num1>/<int:num2>")
+@app.route('/salespoints', methods=['GET', 'POST'])
+def sales_points():
+    # get all
+    if request == 'GET':
+        points = DB.session.query(SalesPoint).all()
+        point_list = []
+        for point in points:
+            point_dict = {
+                'id': point.id,
+                'name': point.name,
+                'coordinates': points.coordinates,
+            }
+            point_list.append(point_dict)
+        return jsonify(point_list)
+    # create
+    if request == 'POST':
+        point = request.get_json()
+        new_point = SalesPoint(name=point['name'], 
+                coordinates=point['coordinates'],
+                )
+        DB.session.add(new_point)
+        DB.session.commit()
+        return {'status': 200}
+    
+@app.route('/salespoint/edit/<int:id>', methods=['POST', 'DELETE'])
+def product(id):
+    # edit
+    if request == 'POST':
+        data = request.get_json()
+        edit = DB.session.query(SalesPoint).filter_by(id=id).first()
+        edit.name=data['name']
+        edit.coordinates=data['coordinates'] 
+        DB.session.add(edit)
+        DB.session.commit()
+        return {'status': 200}
+    # delete
+    elif request == 'DELETE':
+        data = DB.session.query(SalesPoint).filter_by(id=id).first()
+        DB.session.delete(edit)
+        DB.session.commit()
+        return {'status': 200}
 
 if __name__ == "__main__":
     app.run(debug=True)
